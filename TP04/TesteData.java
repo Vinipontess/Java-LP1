fb985b2d0fff40c04102f19700611c4c201c328f
Vinícius Pontes <148925294+Vinipontess@users.noreply.github.com>
2024-07-25 02:01:49 -0300

    Update TesteData.java

--- TP04/TesteData.java
+++ TP04/TesteData.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+// Feito por Vinícius Pontes Oliva e Eduardo Barbosa
+
 public class TesteData {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
