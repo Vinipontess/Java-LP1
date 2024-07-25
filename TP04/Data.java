94723c4658767e976b798fcf17e8307f131c9eb1
Vinícius Pontes <148925294+Vinipontess@users.noreply.github.com>
2024-07-25 02:00:39 -0300

    Add files via upload

---
+++ TP04/Data.java
@@ -0,0 +1,154 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+    private Scanner sc = new Scanner(System.in);
+
+    public Data() {
+        entraDia();
+        entraMes();
+        entraAno();
+    }
+
+    public Data(int d, int m, int a) {
+        if (validaData(d, m, a)) {
+            this.dia = d;
+            this.mes = m;
+            this.ano = a;
+        } else {
+            System.out.println("Data inválida. Inicializando com data padrão 01/01/2000.");
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 2000;
+        }
+    }
+
+    public void entraDia(int d) {
+        if (d > 0 && d <= 31) {
+            this.dia = d;
+        } else {
+            System.out.println("Dia inválido.");
+        }
+    }
+
+    public void entraMes(int m) {
+        if (m > 0 && m <= 12) {
+            this.mes = m;
+        } else {
+            System.out.println("Mês inválido.");
+        }
+    }
+
+    public void entraAno(int a) {
+        if (a > 0) {
+            this.ano = a;
+        } else {
+            System.out.println("Ano inválido.");
+        }
+    }
+
+    public void entraDia() {
+        int d;
+        do {
+            System.out.print("Digite o dia: ");
+            d = sc.nextInt();
+            if (d > 0 && d <= 31) {
+                this.dia = d;
+            } else {
+                System.out.println("Dia inválido. Tente novamente.");
+            }
+        } while (d <= 0 || d > 31);
+    }
+
+    public void entraMes() {
+        int m;
+        do {
+            System.out.print("Digite o mês: ");
+            m = sc.nextInt();
+            if (m > 0 && m <= 12) {
+                this.mes = m;
+            } else {
+                System.out.println("Mês inválido. Tente novamente.");
+            }
+        } while (m <= 0 || m > 12);
+    }
+
+    public void entraAno() {
+        int a;
+        do {
+            System.out.print("Digite o ano: ");
+            a = sc.nextInt();
+            if (a > 0) {
+                this.ano = a;
+            } else {
+                System.out.println("Ano inválido. Tente novamente.");
+            }
+        } while (a <= 0);
+    }
+
+    public int retDia() {
+        return this.dia;
+    }
+
+    public int retMes() {
+        return this.mes;
+    }
+
+    public int retAno() {
+        return this.ano;
+    }
+
+    public String mostra1() {
+        return String.format("%02d/%02d/%04d", this.dia, this.mes, this.ano);
+    }
+
+    public String mostra2() {
+        String[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
+                          "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        return String.format("%02d/%s/%04d", this.dia, meses[this.mes - 1], this.ano);
+    }
+
+    public boolean bissexto() {
+        if ((this.ano % 4 == 0 && this.ano % 100 != 0) || (this.ano % 400 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int diasTranscorridos() {
+        int[] diasPorMes = {31, (bissexto() ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int dias = 0;
+        for (int i = 0; i < this.mes - 1; i++) {
+            dias += diasPorMes[i];
+        }
+        dias += this.dia;
+        return dias;
+    }
+
+    public void apresentaDataAtual() {
+        DateFormat df = DateFormat.getDateInstance(DateFormat.FULL);
+        Date dataAtual = new Date();
+        System.out.println("Data atual: " + df.format(dataAtual));
+    }
+
+    private boolean validaData(int d, int m, int a) {
+        if (m < 1 || m > 12) return false;
+        if (d < 1 || d > 31) return false;
+        if ((m == 4 || m == 6 || m == 9 || m == 11) && d > 30) return false;
+        if (m == 2) {
+            if (bissexto()) {
+                if (d > 29) return false;
+            } else {
+                if (d > 28) return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ TP04/TesteData.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class TesteData {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Data data = new Data();
+
+        while (true) {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1. Inicializar nova data");
+            System.out.println("2. Alterar dia");
+            System.out.println("3. Alterar mês");
+            System.out.println("4. Alterar ano");
+            System.out.println("5. Mostrar data (formato dd/mm/aaaa)");
+            System.out.println("6. Mostrar data (formato dd/mesPorExtenso/aaaa)");
+            System.out.println("7. Verificar se o ano é bissexto");
+            System.out.println("8. Dias transcorridos no ano");
+            System.out.println("9. Mostrar data atual");
+            System.out.println("10. Sair");
+            System.out.print("Opção: ");
+
+            int opcao = sc.nextInt();
+            sc.nextLine();  // Consumir a nova linha
+
+            switch (opcao) {
+                case 1:
+                    data = new Data();
+                    System.out.println("\nData inicializada: " + data.mostra1());
+                    break;
+                case 2:
+                    System.out.print("Digite o novo dia: ");
+                    int d = sc.nextInt();
+                    data.entraDia(d);
+                    System.out.println("\nNovo dia ajustado: " + data.retDia());
+                    break;
+                case 3:
+                    System.out.print("Digite o novo mês: ");
+                    int m = sc.nextInt();
+                    data.entraMes(m);
+                    System.out.println("\nNovo mês ajustado: " + data.retMes());
+                    break;
+                case 4:
+                    System.out.print("Digite o novo ano: ");
+                    int a = sc.nextInt();
+                    data.entraAno(a);
+                    System.out.println("\nNovo ano ajustado: " + data.retAno());
+                    break;
+                case 5:
+                    System.out.println("\nData no formato dd/mm/aaaa: " + data.mostra1());
+                    break;
+                case 6:
+                    System.out.println("\nData no formato dd/mesPorExtenso/aaaa: " + data.mostra2());
+                    break;
+                case 7:
+                    System.out.println("\nAno " + data.retAno() + " é bissexto? " + data.bissexto());
+                    break;
+                case 8:
+                    System.out.println("\nDias transcorridos até " + data.mostra1() + ": " + data.diasTranscorridos());
+                    break;
+                case 9:
+                    data.apresentaDataAtual();
+                    break;
+                case 10:
+                    System.out.println("Saindo...");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+
+            System.out.println("\nDeseja sair ou voltar ao menu principal?");
+            System.out.println("1. Voltar ao menu principal");
+            System.out.println("2. Sair");
+            System.out.print("Opção: ");
+
+            int continuar = sc.nextInt();
+            if (continuar == 2) {
+                System.out.println("Saindo...");
+                sc.close();
+                return;
+            }
+        }
+    }
+}
